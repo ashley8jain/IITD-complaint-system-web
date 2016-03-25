b70f2b2ed0796b5ac39b1b12ed3fc7492bf4b8b3
Saurabh Mylavaram <magicalme049@gmail.comgit config --global  user.email magicalme049@gmail.com>
2016-03-25 22:39:58 +0530

    names in complaint fixed

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b0a0ca8b251a1e4a$2f33a61eccca66ceeae333ee30de56d332651c9e"
+password="pbkdf2(1000,20,sha512)$a5ac56cc6a6a5ce1$ee38da6de7a6c0621d077af7ffc9afd1d1a927bc"
